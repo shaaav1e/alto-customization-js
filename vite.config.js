de82dc6f52281ee4045e3c114251eeb8a40df4ea
shaaav1e <shaaavie@gmail.com>
2025-04-03 07:03:27 +0500

    Should work on vercel

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
-
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
+  plugins: [tailwindcss()],
   // Base path - use '/' for production, './' for relative paths
   base: "/",
 });
