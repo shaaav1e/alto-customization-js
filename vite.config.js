2d259659410cc7169f05a8a50d27e56481b10efd
shaaav1e <shaaavie@gmail.com>
2025-04-03 07:06:54 +0500

    Images should work

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,4 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
   // Base path - use '/' for production, './' for relative paths
-  base: "/",
 });
