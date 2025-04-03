77d0a22803f8d2afa76cf3143b7d554508b05d36
shaaav1e <shaaavie@gmail.com>
2025-04-03 06:59:39 +0500

    added vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  // Base path - use '/' for production, './' for relative paths
+  base: "/",
 });
